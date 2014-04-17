fce049fe7edb6b51f913e2bdd169ce0d9cb41e3d
hliu <hliu0080@gmail.com>
2014-04-16 23:59:05 -0500

    Clean up

--- apps/admin/modules/user/templates/_form.php
+++ apps/admin/modules/user/templates/_form.php
@@ -63,8 +63,28 @@
 	</fieldset>
 <?php else:?>
 	<fieldset>
-		<input type="hidden" name="sf_method" value="put" />	
-      	<?php echo $form ?>
+		<input type="hidden" name="sf_method" value="put" />
+		<?php 
+			foreach($form as $field){
+				if($field->getName()!='eula' && $field->getName()!='_csrf_token'){
+					print $field->renderRow();
+				}elseif($field->getName() == '_csrf_token'){
+					print $field->render();
+				}elseif($field->getName() == 'eula'){
+					print "<div class='control-group ".($field->hasError()?"error'":"'").">";
+					print $field->renderLabel();
+					print "<div class='controls'><label class='checkbox'>";
+					print "	We strongly recommend you NOT to ask for referrals until you have built a good relationship with the mentor, usually after a few meetings. Also, please dress in business casual to attend our Office Hour events.";
+					print "	<br><br><input class='input-xlarge' type='checkbox' name='edit[eula]' id='edit_eula'".($field->getValue()=='on'?' checked ':'')."><label>I understand</label>";
+  					print "</label>";
+  					print $field->hasError()?("<span class='help-inline'>".$field->renderError()."</span>"):"";
+  					print "</div></div>";
+				} 
+			}
+		
+		
+		
+		?>
       	<div class="form-actions">
       		<?php if($form->getOption('usertype')=='becomeMentee' || $form->getOption('usertype')=='becomeMentor'):?>
       			<input class="btn btn-wuxia btn-primary" type="submit" value="Submit"/>
