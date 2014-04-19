716e7c50e6db5fcfb671a3053627c496eaf6ea39
hliu0080 <hliu0080@gmail.com>
2014-04-19 17:50:24 -0500

    Add event top table

--- lib/form/doctrine/base/BaseEventNotifyForm.class.php
+++ lib/form/doctrine/base/BaseEventNotifyForm.class.php
@@ -15,21 +15,23 @@ abstract class BaseEventNotifyForm extends BaseFormDoctrine
   public function setup()
   {
     $this->setWidgets(array(
-      'id'         => new sfWidgetFormInputHidden(),
-      'event_id'   => new sfWidgetFormInputText(),
-      'mentee_id'  => new sfWidgetFormInputText(),
-      'status'     => new sfWidgetFormInputText(),
-      'created_at' => new sfWidgetFormDateTime(),
-      'updated_at' => new sfWidgetFormDateTime(),
+      'id'              => new sfWidgetFormInputHidden(),
+      'event_id'        => new sfWidgetFormInputText(),
+      'mentee_id'       => new sfWidgetFormInputText(),
+      'mentee_username' => new sfWidgetFormInputText(),
+      'status'          => new sfWidgetFormInputText(),
+      'created_at'      => new sfWidgetFormDateTime(),
+      'updated_at'      => new sfWidgetFormDateTime(),
     ));
 
     $this->setValidators(array(
-      'id'         => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
-      'event_id'   => new sfValidatorInteger(array('required' => false)),
-      'mentee_id'  => new sfValidatorInteger(array('required' => false)),
-      'status'     => new sfValidatorInteger(array('required' => false)),
-      'created_at' => new sfValidatorDateTime(array('required' => false)),
-      'updated_at' => new sfValidatorDateTime(array('required' => false)),
+      'id'              => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'event_id'        => new sfValidatorInteger(array('required' => false)),
+      'mentee_id'       => new sfValidatorInteger(array('required' => false)),
+      'mentee_username' => new sfValidatorString(array('max_length' => 25, 'required' => false)),
+      'status'          => new sfValidatorString(array('max_length' => 10, 'required' => false)),
+      'created_at'      => new sfValidatorDateTime(array('required' => false)),
+      'updated_at'      => new sfValidatorDateTime(array('required' => false)),
     ));
 
     $this->widgetSchema->setNameFormat('event_notify[%s]');
--- lib/form/doctrine/base/BaseEventTopicForm.class.php
+++ lib/form/doctrine/base/BaseEventTopicForm.class.php
@@ -1,32 +1,30 @@
 <?php
 
 /**
- * YocaIndustry form base class.
+ * EventTopic form base class.
  *
- * @method YocaIndustry getObject() Returns the current form's model object
+ * @method EventTopic getObject() Returns the current form's model object
  *
  * @package    yoca
  * @subpackage form
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
  */
-abstract class BaseYocaIndustryForm extends BaseFormDoctrine
+abstract class BaseEventTopicForm extends BaseFormDoctrine
 {
   public function setup()
   {
     $this->setWidgets(array(
       'id'   => new sfWidgetFormInputHidden(),
       'name' => new sfWidgetFormInputText(),
-      'type' => new sfWidgetFormInputText(),
     ));
 
     $this->setValidators(array(
       'id'   => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
-      'name' => new sfValidatorString(array('max_length' => 45)),
-      'type' => new sfValidatorString(array('max_length' => 45, 'required' => false)),
+      'name' => new sfValidatorString(array('max_length' => 45, 'required' => false)),
     ));
 
-    $this->widgetSchema->setNameFormat('yoca_industry[%s]');
+    $this->widgetSchema->setNameFormat('event_topic[%s]');
 
     $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
 
@@ -37,7 +35,7 @@ abstract class BaseYocaIndustryForm extends BaseFormDoctrine
 
   public function getModelName()
   {
-    return 'YocaIndustry';
+    return 'EventTopic';
   }
 
 }
